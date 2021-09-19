0f4f43fbabb09760f7ed1f7acd8792d642defc68
Bartosz Bień <bartekb340@gmail.com>
2021-09-19 16:58:09 +0200

    gitignore added, more comments

--- computer_prepare.cpp
+++ computer_prepare.cpp
@@ -1,9 +1,30 @@
+// =========================
+//
+//  Author: Bartosz Bien
+//
+//  These methods run when the user decides to play singleplayer
+//  That means he wants to play with the computer, so the computer has to place somewhere its ships
+//  All the methods inside this file are used during placing computer's ships
+//
+// =========================
+
 #include "classes.hpp"
 #include "includes.hpp"
 #include "defines.hpp"
 
 
-
+// =========================
+//
+//  void my_computer::createComputerBoard() - this method creates computer's board
+//                                            The computer's board is held in \iComputerBoard[][]\ - see \classes.hpp\ for details
+//                                            This method invokes method void my_computer::randomShips(int) to generate one ship with a specified size
+//                                            This method invokes method void my_computer::saveComputerBoard() to save the board to a .txt file
+//
+//    INPUT:  none
+//    OUTPUT: none
+//    NOTE:   This function requires \ctime\ and \cstdio\ because of using pseudo-generating numbers
+//
+// =========================
 void my_computer::createComputerBoard()
 {
     // I clear the board and then fulfill with ships
@@ -12,7 +33,7 @@ void my_computer::createComputerBoard()
             iComputerBoard[i][j] = 0;
 
     // i prepare to generate positions for the ships
-    srand(time(NULL)); // requires ctime, cstdio
+    srand(time(NULL));
 
     // this loop creates 4 sizes of ships
     // the nested one creates 5-n ships for n-sized ship
@@ -24,10 +45,21 @@ void my_computer::createComputerBoard()
     saveComputerBoard();
 }
 
-
+// =========================
+//
+//  void my_computer::randomShips(int iNumOfFields) - this method is used to generate one ship on the board
+//                                                    The ship's size is specified in the variable \iNumOfFields\
+//
+//    INPUT:  iNumOfFields - it carries the requested number of fields which should be occupied by a ship
+//    OUTPUT: none
+//
+// =========================
 void my_computer::randomShips(int iNumOfFields)
 {
-    int iFieldX, iFieldY, iDirection; // direction: 0 - to the right, 1 downwards
+    // iFieldX keeps the position (like on the map) of the beginning of the ship
+    // iFieldY keeps the position but depended on the other axis
+    // iDirection means whether the ship is horizontally (0) or upright (1)
+    int iFieldX, iFieldY, iDirection;
     int iTemp;
 
     while (true)
@@ -36,13 +68,16 @@ void my_computer::randomShips(int iNumOfFields)
         iFieldX = rand() % BOARD_SIZE;
         iFieldY = rand() % BOARD_SIZE;
 
+        // this prevents from generating a ship which would stick out behind the board
         while (iFieldX > (BOARD_SIZE - iNumOfFields))
             iFieldX --;
         while (iFieldY > (BOARD_SIZE - iNumOfFields))
             iFieldY --;
 
+        // now i choose the direction
         iDirection = rand() % 2;
 
+        // and i generate the ship depend on the direction
         if (iDirection == 0)
             for (int i = 0; i < iNumOfFields; i++)
                 if (iComputerBoard[iFieldX + i][iFieldY] == 1)
@@ -63,20 +98,25 @@ void my_computer::randomShips(int iNumOfFields)
         {
             if (iDirection == 0)
                 for (int i = 0; i < iNumOfFields; i++)
-                {
                     iComputerBoard[iFieldX + i][iFieldY] = 1;
-                }
             else
                 for (int i = 0; i < iNumOfFields; i++)
-                {
                     iComputerBoard[iFieldX][iFieldY + i] = 1;
-                }
             return;
         }
     }
 }
 
-
+// =========================
+//
+//  void my_computer::saveComputerBoard() - this method simply prints the computer's board to a .txt file
+//                                          Inside the file 1 means \there's a ship\ and 0 means \no ship\
+//
+//    INPUT:  none
+//    OUTPUT: none
+//    NOTE:   This method requires \fstream\ because of redirection of the output stream
+//
+// =========================
 void my_computer::saveComputerBoard()
 {
     ofstream output("computer_board.txt");
--- user_prepare.cpp
+++ user_prepare.cpp
@@ -1,16 +1,40 @@
+// =========================
+//
+//  Author: Bartosz Bien
+//
+//  These methods run when the user decides to play singleplayer everything works after generating computer's table
+//  All these methods work until the user sets all his ships on the board
+//  The main purpose of the methods is to set user's ships on the board
+//
+// =========================
+
 #include "classes.hpp"
 #include "includes.hpp"
 #include "defines.hpp"
 
 
+// =========================
+//
+//  void my_user::userPosition() - this method is responsible to put user's ships on the board
+//                                 The board when data is saved is called \userBoard[][]\ and there:
+//                                   0 - no ship
+//                                   1 - there's a ship
+//                                   2 - ship was but it was hit
+//                                 We can assume that there are ships: 4x1 field, 3x2 fileds, 2x3 fields, 1x4 fields
+//                                 The number of the ships is saved in \userShipsNumber[]\
+//
+//    INPUT:  none
+//    OUTPUT: none
+//
+// =========================
 void my_user::userPosition()
 {
-    //int userBoard[BOARD_SIZE][BOARD_SIZE] = {0}; // 0 - no ship, 1 - there is a ship, 2 - it was hit
-    //int userShipsNumber[5] = {0}; // 4x1 field, 3x2 fileds, 2x3 fields, 1x4 fields
+    // i clear the table = set all the fileds to no ship
     for (int i = 0; i < BOARD_SIZE; i++)
         for (int j = 0; j < BOARD_SIZE; j++)
           userBoard[i][j] = 0;
 
+    // i set that there is no ship
     for (int i = 0; i < 5; i++)
         userShipsNumber[i] = 0;
 
@@ -18,6 +42,7 @@ void my_user::userPosition()
     int iTypeOfShip;
     int iHowManyShipsRemain = 10;
 
+    // i generate ships when they are left
     while(iHowManyShipsRemain)
     {
         system("cls");
@@ -26,6 +51,7 @@ void my_user::userPosition()
 
         cout << "\n\nWspolrzedna poczatku -> enter -> wspolrzedna konca:\n";
 
+        // i beg user to write the positions of the ship
         usersShips(&iPositionNumber, &iPositionSign);
         iNum1 = iPositionNumber;
         iSign1 = iPositionSign;
@@ -41,12 +67,15 @@ void my_user::userPosition()
         if (iSign1 < iSign2)
             swap(iSign1, iSign2);
 
+        // i check whether the input is correct
         if ((iNum1 >= 0 && iNum1 < BOARD_SIZE && iSign1 >= 0 && iSign1 < BOARD_SIZE) &&
             (iNum2 >= 0 && iNum2 < BOARD_SIZE && iSign2 >= 0 && iSign2 < BOARD_SIZE) &&
-            (iNum1 == iNum2 || iSign1 == iSign2)) // it needs to be improved eg. sign the first, next number or two times the same ship
+            (iNum1 == iNum2 || iSign1 == iSign2))
         {
+            // i check the length of the ship in input
             iTypeOfShip = lengthValidator(iNum1, iNum2, iSign1, iSign2);
 
+            // it means input is correct so the ship can be put
             if (iTypeOfShip)
             {
                 userBoard[iNum1][iSign1] = 1;
@@ -65,28 +94,34 @@ void my_user::userPosition()
                 userShipsNumber[iTypeOfShip]++;
                 iHowManyShipsRemain--;
             }
-
         }
     }
 
+    // this is a kind of the AI - the computer never checks the same field, so i need to set all fields to false
     for (int i = 0; i < BOARD_SIZE; i++)
         for (int j = 0; j < BOARD_SIZE; j++)
           ifFieldCheckedByComp[i][j] = false;
-
-
 }
 
 
-
-
+// =========================
+//
+//  void my_user::usersShips(int*, int*) - this method takes the user input and forwards to void my_user::userPosition()
+//
+//    INPUT:  two pointers which points to the value, coordinates wrote by the user
+//    OUTPUT: none explicity
+//
+// =========================
 void my_user::usersShips(int* iPositionNumber, int* iPositionSign)
 {
     int a, b;
 
-    string choice; // requires cstring
+    // user writes the string with numbers
+    string choice;
     choice.clear();
     cin >> choice;
 
+    // and then the string is transformed into the proper format
     a = ((int) choice[0] - '0') * 10 + (int) choice[1] - '0';
     a--;
     *iPositionNumber = a;
@@ -96,33 +131,35 @@ void my_user::usersShips(int* iPositionNumber, int* iPositionSign)
 }
 
 
+// =========================
+//
+//  int my_user::lengthValidator(int, int, int, int) - this method checks what the ship length is and returns the length
+//
+//    INPUT:  four ints which are coordinates of the ship
+//    OUTPUT: length of the ship
+//
+// =========================
 int my_user::lengthValidator(int iNum1, int iNum2, int iSign1, int iSign2)
 {
+    // -1 means there's error when the value doesn't change
     int iLength = -1;
 
+    // the same numbers = different signs
     if (iNum1 == iNum2)
     {
         iLength = iSign1 - iSign2 + 1;
         if (userShipsNumber[iLength] == (5 - iLength) || iLength > 4)
-        {
             return 0;
-        }
         else
-        {
             return iLength;
-        }
     }
+    // the same signs = different numbers
     else
     {
         iLength = iNum1 - iNum2 + 1;
         if (userShipsNumber[iLength] == (5 - iLength) || iLength > 4)
-        {
             return 0;
-        }
         else
-        {
             return iLength;
-        }
     }
 }
-
